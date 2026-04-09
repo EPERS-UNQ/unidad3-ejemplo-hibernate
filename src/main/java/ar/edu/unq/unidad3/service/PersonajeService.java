734fbb89dd0a16a9c02269831392bf0c632a2d2d
pablov1993 <oronopablo@hotmail.com.ar>
2026-04-09 13:00:05 -0300

    Separo los service de Personaje e Item

--- src/main/java/ar/edu/unq/unidad3/service/PersonajeService.java
+++ src/main/java/ar/edu/unq/unidad3/service/PersonajeService.java
@@ -2,19 +2,16 @@ package ar.edu.unq.unidad3.service;
 
 import ar.edu.unq.unidad3.modelo.Item;
 import ar.edu.unq.unidad3.modelo.Personaje;
+
 import java.util.Collection;
 
-public interface InventarioService {
-    Collection<Item> allItems();
-    Item heaviestItem();
-    void guardarItem(Item item);
+public interface PersonajeService {
     void guardarPersonaje(Personaje personaje);
     Personaje recuperarPersonaje(Long personajeId);
     void recoger(Long personajeId, Long itemId);
     void eliminarPersonaje(Personaje personaje);
-    void eliminarItem(Item item);
+    void eliminarTodos();
 
-    void eliminarTodo();
-    Collection<Item> getMasPesdos(int peso);
-    Collection<Item> getItemsPersonajesDebiles(int vida);
+
+
 }
